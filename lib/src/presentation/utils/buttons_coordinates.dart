32d0a13d1c88df11c24ede9dd6077e441bc9c14c
Viktor Pinkin <viktor.pinkin@nixsolutions.com>
2023-01-14 19:22:03 +0200

    Implemented table widget

---
+++ lib/src/presentation/utils/buttons_coordinates.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+
+const sixTableCoordinates = [
+  Point(176, 50),
+  Point(307, 75),
+  Point(318, 153),
+  Point(176, 200),
+  Point(37, 153),
+  Point(46, 75),
+];
+
+const nineTableCoordinates = [
+  Point(240, 50),
+  Point(307, 75),
+  Point(323, 125),
+  Point(287, 177),
+  Point(178, 200),
+  Point(72, 177),
+  Point(35, 125),
+  Point(50, 75),
+  Point(118, 50),
+];
